dee26da77a8a6cd91aa1b6c63d98411f098633af
Rafa Miranda <rafaelmiranda749@gmail.com>
2022-08-18 10:40:04 -0300

    Se solucionó el scroll Horizontal que tenía para mobile

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const sr = ScrollReveal ({
 	reset: true
 })
 
-sr.reveal('.home-text',{delay:350, origin:'right'})
+sr.reveal('.home-text',{delay:350, origin:'bottom'})
 sr.reveal('.home-img',{delay:350, origin:'top'})
 
 sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
