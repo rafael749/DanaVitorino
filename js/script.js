6416fc307ea5b0dbf6416877486e3fe0c1ac8e12
Rafa Miranda <rafaelmiranda749@gmail.com>
2022-09-03 05:16:28 -0300

    Se carga la primera version del nuevo diseño

--- js/script.js
+++ js/script.js
@@ -26,4 +26,107 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:350, origin:'bottom'})
 sr.reveal('.home-img',{delay:350, origin:'top'})
 
-sr.reveal('.about,.editorial,.contact',{delay:200, origin:'bottom'})
+/* sr.reveal('.about,.editorial,.contact',{delay:200, origin:'bottom'})*/
+
+
+/* 
+			S W I P P E R     J S
+
+*/
+
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	loop: true,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+
+  
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	  1240: {
+		slidesPerView: 4,
+		spaceBetween: 50,
+	  },
+	} 
+    });
+
+
+	var swiper = new Swiper('.swiper-containerAbout', {
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev'
+		},
+		slidesPerView: 1,
+		spaceBetween: 10,
+		loop: true,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+	
+	  
+		breakpoints: {
+		  620: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		  },
+		  680: {
+			slidesPerView: 1,
+			spaceBetween: 40,
+		  },
+		  920: {
+			slidesPerView: 1,
+			spaceBetween: 40,
+		  },
+		  1240: {
+			slidesPerView: 1,
+			spaceBetween: 50,
+		  },
+		} 
+		});	
+
+
+/*
+
+Project - Album
+
+*/		
+
+const images = document.querySelectorAll('.img');
+const containerImage = document.querySelector('.container-img');
+const imageContainer = document.querySelector('.img-show');
+
+images.forEach(image => {
+	image.addEventListener('click', ()=> {
+
+		addImage(image.getAttribute('src'));
+	})
+})
+
+const addImage = (srcImage)=>{
+	containerImage.classList.toggle('move');
+	imageContainer.src = srcImage;
+	console.log(image.getAttribute('src'));
+}
+
+containerImage.addEventListener('click',()=>{
+	containerImage.classList.toggle('move');
+})
