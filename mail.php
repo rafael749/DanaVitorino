84955e4807ae6ff4c8e04267561860e1bf0079bc
Rafa Miranda <rafaelmiranda749@gmail.com>
2022-09-14 08:13:02 -0300

    Puesta en Produccion v1

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
-/*
+
 if($_SERVER['REQUEST_METHOD'] != 'POST' ){
-    header("Location: http://cliente14-mirandawebsoluciones-com.preview-domain.com/" );
+    header("Location: https://danavitorino.com.ar" );
 }
 
 require 'phpmailer/PHPMailer.php';
@@ -16,26 +16,28 @@ $mensaje = $_POST['mensaje'];
 if( empty(trim($nombre)) ) $nombre = 'anonimo';
 
 $body = <<<HTML
-    <h1>Contacto desde la web</h1>
-    <p>De: $nombre / $email</p>
+    <h1>Mensaje enviado desde Danavitorino.com.ar</h1>
+    <p>Nombre Contacto: $nombre / Su e-mail: $email</p>
     <h2>Mensaje</h2>
     $mensaje
 HTML;
 
 $mailer = new PHPMailer();
 $mailer->setFrom( $email, "$nombre" );
-$mailer->addAddress('rafaelmiranda749@gmail.com','Sitio web');
-$mailer->Subject = "Mensaje web: $asunto";
+$mailer->addAddress('info@danavitorino.com.ar','Consultas danavitorino.com.ar');
+$mailer->Subject = "Consulta Nueva";
 $mailer->msgHTML($body);
 $mailer->AltBody = strip_tags($body);
 $mailer->CharSet = 'UTF-8';
 
 $rta = $mailer->send( );
 
-var_dump($rta);
-header("Location: http://cliente14-mirandawebsoluciones-com.preview-domain.com/" );
-*/
+//var_dump($rta);
+echo "<script>alert('Correo enviado Exitosamente')</script>";
+echo "<script>setTimeout(\"location.href='https://danavitorino.com.ar'\",500)</script>";
+//header("Location: https://danavitorino.com.ar" );
 
+/*
 $emailDestino = 'rafaelmiranda749@gmail.com';
 
 $nombre = $_POST['nombre'];
@@ -45,6 +47,7 @@ $mensaje = $_POST['mensaje'];
 $header = "Enviado desde DanaVitorino.com";
 $mensajeEnviado= $mensaje. "\nAtentamente: " . $nombre;
 
-mail($emailDestino, $mensajeEnviado,$header);
+mail($emailDestino, $nombre,$mensajeEnviado,$header);
 echo "<script>alert('Correo enviado Exitosamente')</script>";
-echo "<script>setTimeOut(\"location.href='index.html'\",1000)</script>";
+echo "<script>setTimeOut(\"Location:'https://mirandawebsoluciones.com/danavitorino'\",1000)</script>";
+*/
